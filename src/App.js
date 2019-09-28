1287384f345803694f1188805fb1cba087f5fcf7
vivek <viveksingh101994@gmail.com>
2019-09-29 00:25:48 +0530

    Signin Page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route exact path="/sigin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
