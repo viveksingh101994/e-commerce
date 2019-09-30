5fd0ff55e31364c35ed7aa25b1eeb971102fb157
vivek <viveksingh101994@gmail.com>
2019-09-30 22:59:27 +0530

    NODE_Env added

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,11 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+let middlewares = [];
 
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
